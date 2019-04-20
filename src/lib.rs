5744bbd3cf0e955aa686fec5c1f516935a632beb
Lowell <lowell.mower@gmail.com>
2019-04-20 10:20:57 -0400

    Commit introduces verbose path calling
    This commit cleans up a bit and adds full path names to the function
    calls in the lib.rs file. We do this to be explicit about the calls
    we are making to avoid overlap in the user space when lovely people
    use this crate in their code.
    
    Changes to be committed:
      modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,28 +2,27 @@ extern crate proc_macro;
 extern crate proc_macro2;
 
 use proc_macro::TokenStream;
-use quote::quote;
 use syn::*;
 
 #[proc_macro_attribute]
 pub fn apply_fsdb(_metadata: proc_macro::TokenStream, input: proc_macro::TokenStream)
     -> proc_macro::TokenStream {
     let input: proc_macro2::TokenStream = input.into();
-    let output = quote! {
+    let output = quote::quote! {
         use serde::{Serialize, Deserialize};
         #[derive(Serialize, Deserialize)]
         #input
     };
+
     output.into()
 }
 
 #[proc_macro_derive(JSONable)]
 pub fn _v0_1_0_jsonable(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as syn::DeriveInput);
     let identity = &input.ident;
-    // let data = &input.data;
 
-    let result = quote! {
+    let output = quote::quote! {
         impl #identity {
             fn fsdb_json(&self) -> String {
                 serde_json::to_string_pretty(&self).unwrap().to_string()
@@ -31,5 +30,5 @@ pub fn _v0_1_0_jsonable(input: TokenStream) -> TokenStream {
         }
     };
 
-    result.into()
+    output.into()
 }
